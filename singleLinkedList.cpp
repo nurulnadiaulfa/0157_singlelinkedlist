8834a8ad46a1701a56b94c7444966b7c943fe306
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-08 10:15:50 +0700

    menambahkan void addnode

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -18,4 +18,14 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+    }
 }
